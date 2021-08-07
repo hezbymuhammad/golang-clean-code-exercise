d9fd397cbfdc93cc0a2d30086949519a372b946e
hezbymuhammad <hezbymuhammad@gmail.com>
2021-08-08 00:22:37 +0700

    use transaction on other models

--- models/expenses.go
+++ models/expenses.go
@@ -1,21 +1,5 @@
 package models
 
-type expenses struct {
-        data map[int]int
-}
-
-// TODO: instantiate from transaction model
 func NewExpenses(d []int) Transactions {
-        data := make(map[int]int)
-
-        for i, v := range d {
-                data[i] = v
-        }
-
-        // i.e. return NewTransactions(d)
-        return &expenses{data: data}
-}
-
-func (e *expenses) Get(idx int) int {
-        return e.data[idx]
+        return NewTransactions(d)
 }
--- models/sales.go
+++ models/sales.go
@@ -1,21 +1,5 @@
 package models
 
-type sales struct {
-        data map[int]int
-}
-
-// TODO: instantiate from transaction model
 func NewSales(d []int) Transactions {
-        data := make(map[int]int)
-
-        for i, v := range d {
-                data[i] = v
-        }
-
-        // i.e. return NewTransactions(d)
-        return &sales{data: data}
-}
-
-func (s *sales) Get(idx int) int {
-        return s.data[idx]
+        return NewTransactions(d)
 }
